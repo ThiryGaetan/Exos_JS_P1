f066679665d31d8e017e502a8fe5ab7fbdd9beea
ThiryGaetan <gaetan.thiry@outlook.fr>
2021-04-29 14:59:47 +0200

    fix ex5

--- Exercice5/assets/js/script.js
+++ Exercice5/assets/js/script.js
@@ -1,5 +1,5 @@
 function division(){
     let divisionDividend = document.getElementById('dividend').value;
     let divisionDivisor = document.getElementById('divisor').value;
-    alert('Résultat : '+divisionDividend / divisionDivisor);
+    alert('Résultat : '+divisionDividend % divisionDivisor);
 }
